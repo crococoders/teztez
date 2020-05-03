24de49e45221eeef4b020b571e82e99482424009
Adlet <160103098@stu.sdu.edu.kz>
2020-05-03 17:58:34 +0600

    feat: add BlenderConfiguration

---
+++ Client/Teztez/Flows/Blender/Modules/BlenderConfiguration/Store/BlenderConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  BlenderConfiguration.swift
+//  Teztez
+//
+//  Created by Adlet on 5/3/20.
+//  Copyright © 2020 crococoders. All rights reserved.
+//
+
+import UIKit
+
+struct BlenderConfiguration {
+    let text: String
+    let fontSize: CGFloat
+}
