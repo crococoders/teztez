c930b1d326ff4a3632074fb8c2e71e3e06cd69a4
Adlet <160103098@stu.sdu.edu.kz>
2020-05-03 21:11:46 +0600

    feat: add Blender game

--- Client/Teztez/Flows/Blender/Coordinator/BlenderCoordinator.swift
+++ Client/Teztez/Flows/Blender/Coordinator/BlenderCoordinator.swift
@@ -22,6 +22,8 @@ final class BlenderCoordinator: Coordinator, BlenderCoordinatorOutput {
     private let moduleFactory: BlenderModuleFactory
     private let router: Router
     private var blenderConfiguration: BlenderConfigurationPresentable?
+    private var configuration: BlenderConfiguration?
+    private var fonSize: CGFloat?
 
     init(moduleFactory: BlenderModuleFactory, router: Router) {
         self.moduleFactory = moduleFactory
@@ -48,6 +50,63 @@ final class BlenderCoordinator: Coordinator, BlenderCoordinatorOutput {
         blenderConfiguration?.onCloseButtonDidTap = { [weak self] in
             self?.onFlowDidFinish?()
         }
+        blenderConfiguration?.onFontSizeChangeDidTap = { [weak self] in
+            guard let self = self else { return }
+            let fontSize = self.fonSize ?? Constants.defaultFontSize
+            self.showFontSizeChange(fontSize: fontSize)
+        }
+        blenderConfiguration?.onTextInputDidTap = { [weak self] in
+            self?.showInpuText(text: nil)
+        }
+        blenderConfiguration?.onStartButtonDidTap = { [weak self] configuration in
+            guard let self = self else { return }
+            self.configuration = configuration
+            self.showBlenderConvert(configuration: configuration)
+        }
+        blenderConfiguration?.onContinueButtonDidTap = { [weak self] in
+            guard
+                let self = self,
+                let configuration = self.configuration else { return }
+            self.showBlenderConvert(configuration: configuration)
+        }
         router.setRootModule(blenderConfiguration)
+    }
+
+    private func showInpuText(text: String?) {
+        var (container, inputText) = moduleFactory.makeBlenderTextInput(text: text)
+        inputText.onCancelButtonDidTap = { [weak self] in
+            self?.router.dismissModule()
+        }
+        inputText.onDoneButtonDidTap = { [weak self] text in
+            guard let self = self else { return }
+            self.blenderConfiguration?.setUserText(text)
+            self.router.dismissModule()
+        }
+        router.show(container, with: .presentInSheet(dismissable: false))
+    }
+
+    private func showFontSizeChange(fontSize: CGFloat) {
+        var (container, fontSizeChange) = moduleFactory.makeBlenderFontSizeChange(fontSize: fontSize)
+        fontSizeChange.onCancelButtonDidTap = { [weak self] in
+            self?.router.dismissModule()
+        }
+
+        fontSizeChange.onFontSizeDidSelect = { [weak self] fontSize in
+            guard let self = self else { return }
+            self.fonSize = fontSize
+            self.blenderConfiguration?.setUserFontSize(fontSize)
+            self.router.dismissModule()
+        }
+        router.show(container, with: .presentInSheet(dismissable: false))
+    }
+
+    private func showBlenderConvert(configuration: BlenderConfiguration) {
+        var blenderConvert = moduleFactory.makeBlenderConvert(configuration: configuration)
+        blenderConvert.onBackButtonDidTap = { [weak self] in
+            guard let self = self else { return }
+            self.blenderConfiguration?.setupBlenderPause()
+            self.router.popModule()
+        }
+        router.show(blenderConvert, with: .push)
     }
 }
--- Client/Teztez/Flows/Blender/Coordinator/BlenderModuleFactory.swift
+++ Client/Teztez/Flows/Blender/Coordinator/BlenderModuleFactory.swift
@@ -6,11 +6,14 @@
 //  Copyright © 2020 crococoders. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 protocol BlenderModuleFactory {
     func makeBlenderActivityIntro() -> ActivitiesIntroPresentable
     func makeBlenderConfiguration() -> BlenderConfigurationPresentable
+    func makeBlenderTextInput(text: String?) -> (container: CoordinatorNavigationController, module: TextInputPresentable)
+    func makeBlenderFontSizeChange(fontSize: CGFloat) -> (container: CoordinatorNavigationController, module: FontSizeChangePresentable)
+    func makeBlenderConvert(configuration: BlenderConfiguration) -> BlenderConvertPresentable
 }
 
 extension ModuleFactory: BlenderModuleFactory {
@@ -23,6 +26,27 @@ extension ModuleFactory: BlenderModuleFactory {
     func makeBlenderConfiguration() -> BlenderConfigurationPresentable {
         let store = BlenderConfigurationStore()
         let viewController = BlenderConfigurationViewController(store: store)
+        return viewController
+    }
+
+    func makeBlenderTextInput(text: String?) -> (container: CoordinatorNavigationController, module: TextInputPresentable) {
+        let viewController = TextInputViewController(store: TextInputStore())
+        let navigaitonController = CoordinatorNavigationController()
+        navigaitonController.setViewControllers([viewController], animated: true)
+        return (navigaitonController, viewController)
+    }
+
+    func makeBlenderFontSizeChange(fontSize: CGFloat) -> (container: CoordinatorNavigationController, module: FontSizeChangePresentable) {
+        let store = FontSizeChangeStore(fontSize: fontSize)
+        let viewController = FontSizeChangeViewController(store: store)
+        let navigaitonController = CoordinatorNavigationController()
+        navigaitonController.setViewControllers([viewController], animated: true)
+        return (navigaitonController, viewController)
+    }
+
+    func makeBlenderConvert(configuration: BlenderConfiguration) -> BlenderConvertPresentable {
+        let store = BlenderConvertStore(configuration: configuration)
+        let viewController = BlenderConvertViewController(store: store)
         return viewController
     }
 }
